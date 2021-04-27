3976fc79c55e10910b0f965f9edf5cb6086cb3c7
Christina <73680467+christinna9031@users.noreply.github.com>
2021-04-27 01:23:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,33 +8,20 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(xhub({algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
-var received_updates = [];
+
 
 app.get('/', function(req, res) {
-  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+  res.send('This the simple Webhook server sample.  Use POST methods instead of GET.');
 });
 
 app.get('/webhooks', function(req, res) {
-  if (req.param('hub.mode') != 'subscribe'
-      || req.param('hub.verify_token') != process.env.VERIFY_TOKEN) {
-    res.sendStatus(401);
-    return;
-  }
-
-  res.send(req.param('hub.challenge'));
+  res.send('This the simple Webhook server sample.  Use POST methods instead of GET.);
 });
 
 app.post('/webhooks', function(req, res) {
-  if (!req.isXHubValid()) {
-    console.log('Received webhooks update with invalid X-Hub-Signature');
-    res.sendStatus(401);
-    return;
-  }
-  console.log(JSON.stringify(req.body, null, 2));
-  received_updates.unshift(req.body);
+  console.log(JSON.stringify(req.body));
   res.sendStatus(200);
 });
 
