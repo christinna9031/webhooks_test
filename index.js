5179af403dfbd252b3a71305c49d0fe5fe3030cf
Kristýna Králová <christinna@hotmail.cz>
2021-04-27 02:34:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
 'use strict'
 
 const express = require('express');
-const path = require('path');
-const { createServer } = require('http');
 const bodyParser = require('body-parser');
+const request = require('request');
 const app = express();
 
-const PORT = 8000;
-const WebSocket = require('ws');
-
-const appW = express();
-appW.use(express.static(path.join(__dirname, '/public')));
-
-const server = createServer(appW);
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', function (ws) {
-  const id = setInterval(function () {
-    ws.send("Hello");
-  }, 1000);
-  console.log('started client interval');
-
-  ws.on('close', function () {
-    console.log('stopping client interval');
-    clearInterval(id);
-  });
-});
-
-server.listen(8080, function () {
-  console.log('Listening on http://0.0.0.0:8080');
-});
-
-
-
-
-
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
@@ -49,7 +20,7 @@ app.get('/webhooks', function(req, res) {
 });
 
 app.post('/webhooks', function(req, res) {
-  console.log(req.body);
+  console.log(JSON.stringify(req.body));
   res.sendStatus(200);
 });
 
